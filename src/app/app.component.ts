70803fb0ef61297fbe7e48fe908cc362e45d3d71
yourgotocoder <yourgotocoder@gmail.com>
2022-09-22 11:27:57 +0530

    Challenge 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,46 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  constructor(private http: HttpClient) {}
 
-  joke: any;
-  punchlineVisible: boolean = false;
-  timer: number = 7200;
-  hours: number = Math.floor(this.timer / 3600);
-  minutes: number = Math.floor((this.timer % 3600) / 60);
-  seconds: number = Math.floor((this.timer - this.minutes * 60) % 60);
-
-  ngOnInit(): void {
-    this.http
-      .get('https://official-joke-api.appspot.com/random_joke')
-      .subscribe((data) => (this.joke = data));
-  }
-
-  startTimer = () => {
-    if (this.timer > 0) {
-      setInterval(() => {
-        this.timer--;
-        this.hours = Math.floor(this.timer / 3600);
-        this.minutes = Math.floor((this.timer % 3600) / 60);
-        this.seconds = Math.floor((this.timer - this.minutes * 60) % 60);
-      }, 1000);
-    }
-  };
-
-  formatTime = (value: number): string => {
-    if (value < 10) {
-      return `0${value}`;
-    } else return `${value}`;
-  };
-
-  getJoke = () => {
-    this.punchlineVisible = false;
-    this.http
-      .get('https://official-joke-api.appspot.com/random_joke')
-      .subscribe((data) => (this.joke = data));
-  };
-
-  showPunchline = () => {
-    this.punchlineVisible = true;
-  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,14 +7,16 @@ import { TodoComponent } from './todo/todo.component';
 import { TodoAddComponent } from './todo/todo-add/todo-add.component';
 import { TodoListComponent } from './todo/todo-list/todo-list.component';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from "@angular/common/http"
+import { HttpClientModule } from "@angular/common/http";
+import { CalculatorComponent } from './calculator/calculator.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
     TodoAddComponent,
-    TodoListComponent
+    TodoListComponent,
+    CalculatorComponent
   ],
   imports: [
     BrowserModule,
